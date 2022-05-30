aab5d579dbc18c554430b574b7bd41d1f988628b
Wylie Mickelson <wyliecoyote910@gmail.com>
2022-05-30 13:05:06 -0700

    refactored guessing and input validation

--- lib/game.rb
+++ lib/game.rb
@@ -18,8 +18,22 @@ class Game
   end
 
   def setup
-    # puts display_start
+    puts display_start
+    get_game_type
     create_code
+  end
+
+  def get_game_type
+    type = gets.chomp
+    until valid_game_type?(type)
+      puts display_invalid_game_type
+      type = gets.chomp
+    end
+  end
+
+  def valid_game_type?(type)
+    valid_types = "12"
+    valid_types.include?(type) && type != ""
   end
 
   def game_over?
@@ -29,32 +43,37 @@ class Game
     elsif board.sequences.length == 12
       puts display_cm_win
       return true
+    elsif curr_guess == "q"
+      puts display_tfp
+      return true
     end
     false
   end
 
   def create_code
     print display_codemaker_prompt.chomp
-    code = get_seq
+    code = get_guess
     @board.code = code
   end
 
-  def get_seq
-    seq = Sequence.new(gets.chomp.upcase.split(""))
-    check_valid_seq(seq)
-  end
-
-  def check_valid_seq(seq)
-    until valid_seq?(seq)
+  def get_guess
+    guess = Sequence.new(gets.chomp.upcase.split(""))
+    until valid_guess?(guess)
       puts display_invalid_sequence
-      seq = get_seq
+      guess = Sequence.new(gets.chomp.upcase.split(""))
     end
-    seq
+    guess
   end
-
-  def valid_seq?(seq)
+  
+  def valid_guess?(guess)
     valid_colors = "RCYGBM"
-    if seq.nil? || seq.colors.length != 4 || seq.colors.any? { |color| !valid_colors.include?(color) }
+    if guess.nil? || guess.empty?
+      return false
+    end
+    if guess.colors[0].downcase == 'q'
+      return true
+    end
+    if guess.colors.length != 4 || guess.colors.any? { |color| !valid_colors.include?(color) }
       return false
     end
     true
@@ -62,7 +81,7 @@ class Game
 
   def turn
     print display_turn_prompt
-    guessed_seq = get_seq
+    guessed_seq = get_guess
     board.add_sequence(guessed_seq)
     clear
     board.display
